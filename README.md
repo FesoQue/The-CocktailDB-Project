c465bbae08fc60167fc266f8ccba8f6cd9e5ea65
FesoQue <adefesoq@gmail.com>
2022-01-02 17:49:38 +0100

    cocktail DB project concluded

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# The-CocktailDB-Project
+#### React Router Fix
+
+(Fix)[https://dev.to/dance2die/page-not-found-on-netlify-with-react-router-58mc]
+
+#### CRA Fix
+
+```
+
+"build": "CI= react-scripts build",
+
+```
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem
+        voluptatibus quod id natus, explicabo dolorum fugiat aperiam repellat
+        beatae culpa quibusdam reprehenderit exercitationem aliquid ipsum iusto
+        cupiditate esse. Alias magni ipsam eaque similique porro nam non
+        perspiciatis labore dolore officia!
+      </p>
+    </section>
+  );
+};
+
+export default About;
